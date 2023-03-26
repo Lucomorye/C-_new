e5af1dbbc5e2286c90e54ad505bb884c487d21fc
Tarantin <mr.fadeyka@gmail.com>
2023-03-26 05:00:29 +0300

    Написали программу Hello Username

--- lectures01/example02_NameUser/Program.cs
+++ lectures01/example02_NameUser/Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("Введите имя: => ");
+string userName = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(userName);
 
